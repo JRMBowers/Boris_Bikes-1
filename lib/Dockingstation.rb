e3cb5c7d413f50406018d0c04075a677c3085254
Jamie Bowers <jamiebowers3@gmail.com>
2021-10-13 17:04:58 +0100

    Up to 17 Complete

--- lib/Dockingstation.rb
+++ lib/Dockingstation.rb
@@ -1,18 +1,42 @@
 require '/Users/jbower34/Desktop/Projects/Boris_Bikes-1/lib/bike.rb'
-class Dockingstation
-    attr_reader :bike
 
-    def initialize
-        @bike_count = 1
+# class Array
+#     def full?
+#         if self.count >= 20 
+#             return true
+#         else 
+#             return false
+#         end 
+#     end 
+# end 
+
+
+class Dockingstation
+    DEFAULT_CAPACITY = 20
+    attr_reader :bike,:capacity
+
+    def initialize(capacity=DEFAULT_CAPACITY)
+        @bike = []
+        @capacity = capacity
     end
 
     def release_bike
-        fail 'No bikes available' unless @bike
-        @bike
+        fail 'No bikes available' if empty?
+        @bike.pop
     end
 
     def dock(bike)
-        @bike = bike
+        fail 'Bike dock full' if full?
+        @bike << bike
     end 
+
+    def full?
+      @bike.count >= @capacity
+    end 
+
+    def empty?
+      @bike.empty?
+    end 
+
 
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -7,33 +7,42 @@ describe Dockingstation do
             subject.dock(bike)
             expect(subject.release_bike).to eq bike
         end  
+        it 'raises error when no bikes are available' do 
+            expect { subject.release_bike }.to raise_error 'No bikes available'
+        end 
 
-        it 'adds a bike to number of bikes released' do
-        #     dockingstation = Dockingstation.new 
-        #     expect{dockingstation.release_bike}.not_to raise_error 
+        it 'checks we can dock and release a bike' do
             bike = Bike.new
             dock = subject.dock(bike)
             subject.release_bike
             expect(bike).to be_working
-        # rescue 
-        #     RSpec::Expectations::ExpectationNotMetError => e
-        #     expect(e.message).not_to include 'AError'
         end 
           
     end
     describe '#dock' do 
         it { is_expected.to respond_to(:dock).with(1).argument }
-    end 
     
-    it 'dock something' do 
-        bike = Bike.new 
-        subject.dock(bike)
-        expect(subject.bike).to eq bike 
-    end 
+        it 'checks when we dock something' do 
+            bike = Bike.new 
+            subject.dock(bike)
+            expect((subject.bike).include?(bike)).to eq true
+        end
 
-    describe '#release_bike' do
-        it 'raises error when no bikes are available' do 
-            expect { subject.release_bike }.to raise_error 'No bikes available'
+        it 'checks for error when dock is full' do
+            bike = Bike.new
+            Dockingstation::DEFAULT_CAPACITY.times { subject.dock(bike) }
+            expect{ subject.dock(bike) }.to raise_error "Bike dock full"
+        end
+
+        it 'checks no error when dock is empty and we add a bike' do 
+            bike = Bike.new
+            expect(subject.dock(bike).count).to eq(1)
         end 
     end 
+    describe 'checks' do
+        it 'check default capacity' do 
+            expect(subject.capacity).to eq Dockingstation::DEFAULT_CAPACITY
+        end
+    end
+    
 end
